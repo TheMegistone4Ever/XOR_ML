90fb0fbc8431c8d1cf7271fcfb5afe0617fc5cfd
TheMegistone4Ever <zeusmobilenick@gmail.com>
2022-07-17 23:52:57 +0300

    Work

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
 from XOR_NL import *
+import time
+start_time = time.time()
+training_inputs = np.array([
+    [0, 0, 0],
+    [0, 0, 1],
+    [0, 1, 0],
+    [0, 1, 1],
+    [1, 0, 0],
+    [1, 0, 1],
+    [1, 1, 0],
+    [1, 1, 1]
+])
 
+training_outputs = np.array([[0, 1, 1, 0, 1, 0, 0, 1]]).T
+
+train(training_inputs, training_outputs)
 run([[0, 1, 1]])
-MSE_grapf()
 show_neural_network()
+print("time elapsed: {:.2f}s".format(time.time() - start_time))
+MSE_grapf()
