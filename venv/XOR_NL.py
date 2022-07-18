e87143332855e42fa9ff433176ff0e716eec3255
TheMegistone4Ever <zeusmobilenick@gmail.com>
2022-07-18 18:28:56 +0300

    Maybe final result

---
+++ venv/XOR_NL.py
@@ -0,0 +1,65 @@
+import matplotlib.pyplot as plt
+import numpy as np
+np.set_printoptions(precision=3, suppress=True)
+np.random.seed(0)
+l = 3
+
+graph_set = {
+    "x_axis" : [],
+    "y_axis" : []
+}
+
+NL = {
+    "layer" : [None]*l,
+    "l_error" : [None]*l,
+    "l_delta" : [None]*l,
+    "synapse" : [None]*(l-1)
+}
+
+def sigmoid(x):
+    return 1/(1+np.exp(-x))
+
+def d_sigmoid(x):
+    return sigmoid(x) * (1 - sigmoid(x))
+
+def train(inputs, outputs, generations=4096):
+    NL["synapse"][0] = 2 * np.random.random((inputs.T).shape) - 1
+    NL["synapse"][1] = 2 * np.random.random(outputs.shape) - 1
+    NL["layer"][0] = inputs
+
+    for i in range(generations):
+        # Layers initialization
+        for j in range(1, l):
+            NL["layer"][j] = sigmoid(np.dot(NL["layer"][j - 1], NL["synapse"][j - 1]))
+
+        # Calculation of layer error and deviation level
+        NL["l_error"][l - 1] = outputs - NL["layer"][l - 1]
+        NL["l_delta"][l - 1] = NL["l_error"][l - 1] * d_sigmoid(NL["layer"][l - 1])
+        for j in range(1, l - 1):
+            NL["l_error"][l - 1 - j] = np.dot(NL["l_delta"][l - j], NL["synapse"][l - 1 - j].T)
+            NL["l_delta"][l - 1 - j] = NL["l_error"][l - 1 - j] * d_sigmoid(NL["layer"][l - 1 - j])
+
+        # Adjusting synopses based on deviation level
+        for j in range(l - 1):
+            NL["synapse"][l - 2 - j] += np.dot(NL["layer"][l - 2 - j].T, NL["l_delta"][l - 1 - j])
+
+        if i % 1000 == 0:
+            graph_set["x_axis"].append(i / 1000)
+            graph_set["y_axis"].append(np.mean(abs(NL["l_error"][l - 1])))
+
+def show_neural_network():
+    print("L0:\n", NL["layer"][0])
+    print("SYNAPCES_01:\n", NL["synapse"][0])
+    print("L1:\n", NL["layer"][1])
+    print("SYNAPCES_12:\n", NL["synapse"][1])
+    print("L2:\n", NL["layer"][2])
+
+def run(input):
+    for data in input:
+        NL["layer"][1] = sigmoid(np.dot(data, NL["synapse"][0]))
+        print("XOR: ", data, " = ", sigmoid(np.dot(NL["layer"][1],NL["synapse"][1])))
+
+def MSE_grapf():
+    plt.plot(graph_set["x_axis"], graph_set["y_axis"], c="black", linewidth=4, marker="o", markersize=6, label="MSE")
+    plt.legend()
+    plt.show()
